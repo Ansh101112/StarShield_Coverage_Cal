e6180b71a09825a7f1502d35529b9181ac384b71
Anshuman <tanshuman145@gmail.com>
2023-10-15 01:51:55 +0530

    first commit

---
+++ Smart Paints/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productSelect = document.getElementById("product");
+    const areaInput = document.getElementById("area");
+    const calculateButton = document.getElementById("calculate");
+    const resultDiv = document.getElementById("result");
+
+    calculateButton.addEventListener("click", function () {
+        const selectedProduct = productSelect.value;
+        const area = parseFloat(areaInput.value);
+
+        const coverageValues = {
+            "StarSmartPaintExterior": 250, 
+            "StarSmartPaintInterior": 250,
+            "StarMosquitoShield": 35,
+            "StarRodentShield": 250,
+            "StarStainSield": 200,
+            "StarVirusShield": 200,
+        };
+
+        if (!isNaN(area)) {
+            if (coverageValues[selectedProduct]) {
+                const coverage = area / coverageValues[selectedProduct];
+                const wholeUnits = Math.ceil(coverage); // Round up to the nearest whole unit
+                resultDiv.textContent = `You will need ${wholeUnits} units of ${selectedProduct} to cover the room area.`;
+            } else {
+                resultDiv.textContent = "Please select a valid product.";
+            }
+        } else {
+            resultDiv.textContent = "Please enter a valid room area.";
+        }
+    });
+});
